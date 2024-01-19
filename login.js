8a90518756ff5f7a3b916af763229d180beb8905
pedri250203 <156392585+pedri250203@users.noreply.github.com>
2024-01-18 22:21:04 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded',
 function(){
-  console.log('Hello, Pedri!'):
+  console.log('Hello, Pedri!');
 
   var imputemail = document.getElementByID('txtusuario');
   var imputpassword = document.getElementByID('txtpassword');
-
+console.log(imputemail);
+console.log(imputpassword);
   
 
 ));
