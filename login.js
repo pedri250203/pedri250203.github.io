0049e04db8a9ddaaddee16e8b4c0ffd4b5660db1
pedri250203 <156392585+pedri250203@users.noreply.github.com>
2024-01-25 21:07:15 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Hello, Pedri!');
 
-    // Función para validar el formulario de inicio de sesión
+  
     function validarLogin() {
         var inputEmail = document.getElementById('txtusuario').value;
         var inputPassword = document.getElementById('txtpassword').value;
 
-        // Validar el formato del correo electrónico y la contraseña
+       
         var emailValido = validarEmail(inputEmail);
         var passwordValido = validarPassword(inputPassword);
 
-        // Mostrar mensajes de error si es necesario
+     
         if (!emailValido) {
             alert('Ingrese un correo electrónico válido.');
             return;
@@ -21,17 +21,17 @@ document.addEventListener('DOMContentLoaded', function () {
             return;
         }
 
-        // Si todo está bien, puedes enviar el formulario o realizar otras acciones
+       
         alert('¡Inicio de sesión exitoso!');
     }
 
-    // Función para validar el formato del email (puedes usar tu propia implementación)
+   
     function validarEmail(email) {
         const expresionRegular = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return expresionRegular.test(email);
     }
 
-    // Función para validar el formato de la contraseña (puedes ajustar según tus requisitos)
+   
     function validarPassword(password) {
         return password.length >= 8;
     }
