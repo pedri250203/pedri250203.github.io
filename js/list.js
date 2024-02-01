249e912214335aa1df79a54b4eda14f9ced5ed31
pedri250203 <156392585+pedri250203@users.noreply.github.com>
2024-02-01 17:49:48 -0600

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,58 +1,31 @@
-function getCharacters(done) {
-
-    const results = fetch("https://rickandmortyapi.com/api/character");
-    results
-        .then(response => response.json())
-        .then(data => {
-            done(data)
-        });
-    
-}
-
-getCharacters(data => {
-    data.results.forEach(personaje => {
-        const article = document.createRange().createContextualFragment(/*html*/`
-
-        <article>
-
-            <div class="image-container">
-                <img src="${personaje.image}" alt="Personaje">
-            </div>
-
-            <h2>${personaje.name}</h2>
-            <span>${personaje.status}</span>
-
-        </article>
-        `);
-
-        const main = document.querySelector("main");
-        
-        main.append(article);
-});
-
-});
-
-    
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
+    let currentIndex = 0; // Variable para rastrear el índice actual del personaje
+
     document.querySelector(".btn.btn-primary").addEventListener("click", event => {
-        var itemList = document.getElementById("personaje");
-        var template = document.getElementById("list-template");
-        var total = itemList.childElementCount + 1;
-        var clone = template.content.cloneNode(true);
-        clone.querySelector("[data-id='number']").textContent = `${total}`;
-        clone.querySelector("[data-id='title']").textContent = "Product";
-        clone.querySelector("[data-id='content']").textContent = "It's a new item";
-        itemList.appendChild(clone);
+        getCharacters(data => {
+            const personaje = data.results[currentIndex];
+
+            if (personaje) {
+                const itemList = document.getElementById("personaje");
+                const template = document.getElementById("list-template");
+                const total = itemList.childElementCount + 1;
+                const clone = template.content.cloneNode(true);
+                clone.querySelector("[data-id='number']").textContent = `${total}`;
+                clone.querySelector("[data-id='title']").textContent = personaje.name;
+                clone.querySelector("[data-id='content']").textContent = personaje.status;
+                itemList.appendChild(clone);
+
+                currentIndex++; // Incrementa el índice para el próximo personaje
+            }
+        });
     });
+
     document.querySelector(".btn.btn-light").addEventListener("click", event => {
-        var itemList = document.getElementById("my-list");
+        var itemList = document.getElementById("personaje");
         itemList.replaceChildren();
+        currentIndex = 0; // Reinicia el índice al principio cuando se borran todos los elementos
     });
 });
-
 
 
     
