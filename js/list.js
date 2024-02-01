73ceb1a7a01d9236a3528a67a1114a4b52a874d5
pedri250203 <156392585+pedri250203@users.noreply.github.com>
2024-02-01 17:39:10 -0600

    Update list.js

--- js/list.js
+++ js/list.js
@@ -10,8 +10,29 @@ function getCharacters(done) {
 }
 
 getCharacters(data => {
-    console.log(data);
+    data.results.forEach(personaje => {
+        const article = document.createRange().createContextualFragment(/*html*/`
+
+        <article>
+
+            <div class="image-container">
+                <img src="${personaje.image}" alt="Personaje">
+            </div>
+
+            <h2>${personaje.name}</h2>
+            <span>${personaje.status}</span>
+
+        </article>
+        `);
+
+        const main = document.querySelector("main");
+        
+        main.append(article);
 });
+
+});
+
+    
 
 
 
