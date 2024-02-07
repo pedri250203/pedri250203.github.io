c5ee7f58bc5a5d1ba71fd72292b61803abd501f5
pedri250203 <156392585+pedri250203@users.noreply.github.com>
2024-02-07 17:43:04 -0600

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {  
+document.addEventListener("DOMContentLoaded", () => {
     let currentIndex = 0; // Variable para rastrear el índice actual del personaje
 
     // Función para obtener los personajes
@@ -7,37 +7,28 @@ document.addEventListener("DOMContentLoaded", () => {
         results
             .then(response => response.json())
             .then(data => {
-                done(data);
+                done(data.results);
             });
     }
 
     document.querySelector(".btn.btn-primary").addEventListener("click", event => {
         getCharacters(data => {
-            const personaje = data.results[currentIndex];
+            const itemList = document.getElementById("personaje");
+            const template = document.getElementById("list-template");
+            itemList.innerHTML = ''; // Limpiar el contenido anterior
 
-            if (personaje) {
-                const itemList = document.getElementById("personaje");
-                const template = document.getElementById("list-template");
-                const total = itemList.childElementCount + 1;
+            data.forEach((personaje, index) => {
                 const clone = template.content.cloneNode(true);
-                
+
                 // Llenar los campos del template con la información del personaje
-                clone.querySelector("[data-id='number']").textContent = `${total}`;
+                clone.querySelector("[data-id='number']").textContent = `${index + 1}`;
                 clone.querySelector("[data-id='title']").textContent = personaje.name;
                 clone.querySelector("[data-id='content']").textContent = `${personaje.status}, ${personaje.species}`;
                 clone.querySelector("[data-id='image']").src = personaje.image;
 
                 itemList.appendChild(clone);
-
-                currentIndex++; // Incrementa el índice para el próximo personaje
-            }
+            });
         });
-    });
-
-    document.querySelector(".btn.btn-light").addEventListener("click", event => {
-        var itemList = document.getElementById("personaje");
-        itemList.replaceChildren();
-        currentIndex = 0; // Reinicia el índice al principio cuando se borran todos los elementos
     });
 });
 
